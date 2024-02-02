2183e157b30bc7517424b532b490c760715a3624
Phillip Clontz <PhillipAC718@gmail.com>
2024-02-02 11:40:33 -0600

    Built

--- src/app/components/help/help.component.ts
+++ src/app/components/help/help.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-help',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './help.component.html',
   styleUrl: './help.component.scss'
 })
